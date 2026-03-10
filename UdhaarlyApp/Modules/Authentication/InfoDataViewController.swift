a9e75fc1f0d1550c802f84e5914c21919bc6dda5
Tadian Ahmad Azeemi <tadianiosdev@gmail.com>
2026-03-10 10:15:00 +0500

    Revert "add page view logic build completed"
    This reverts commit 7185998eaf28ec63fa32c8d54183130560547dc1.

--- UdhaarlyApp/Modules/Authentication/InfoDataViewController.swift
+++ UdhaarlyApp/Modules/Authentication/InfoDataViewController.swift
@@ -264,7 +264,6 @@ class InfoDataViewController: UIViewController {
 
             // 6. Finalize Registration locally
             UserDefaults.standard.set(true, forKey: "isUserLoggedIn")
-            UserDefaults.standard.set(email, forKey: "currentUserEmail") // Persist for auto-fill logic
             
             // Move to Dashboard
             let dashboardVC = DashboardViewController()
