b1ce5ee5fc2a08e03cc28813217bdcddd6bd7e17
Tadian Ahmad Azeemi <tadianiosdev@gmail.com>
2026-03-11 13:04:38 +0500

    Cleaned up the Home screen by removing the extra 'Add Product' button since we now have the main one in the bottom menu. Also finished matching up the background colors across all the main pages.

--- UdhaarlyApp/Modules/Dashboard/DashboardViewController.swift
+++ UdhaarlyApp/Modules/Dashboard/DashboardViewController.swift
@@ -17,16 +17,6 @@ class DashboardViewController: UIViewController {
         return label
     }()
     
-    private let addProductButton: UIButton = {
-        let btn = UIButton(type: .system)
-        btn.setTitle("Add New Product", for: .normal)
-        btn.backgroundColor = .brandOrange
-        btn.setTitleColor(.white, for: .normal)
-        btn.layer.cornerRadius = 15
-        btn.titleLabel?.font = .systemFont(ofSize: 18, weight: .bold)
-        btn.addDropShadow()
-        return btn
-    }()
 
     private var gradientLayer: CAGradientLayer?
     
@@ -37,7 +27,6 @@ class DashboardViewController: UIViewController {
         
         setupLayout()
         
-        addProductButton.addTarget(self, action: #selector(didTapAddProduct), for: .touchUpInside)
     }
     
     override func viewDidLayoutSubviews() {
@@ -47,25 +36,14 @@ class DashboardViewController: UIViewController {
     
     private func setupLayout() {
         view.addSubview(titleLabel)
-        view.addSubview(addProductButton)
         
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        addProductButton.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
             titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 40),
-            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            
-            addProductButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            addProductButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            addProductButton.widthAnchor.constraint(equalToConstant: 200),
-            addProductButton.heightAnchor.constraint(equalToConstant: 55)
+            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
         ])
     }
     
-    @objc private func didTapAddProduct() {
-        let addProductVC = AddProductViewController()
-        navigationController?.pushViewController(addProductVC, animated: true)
-    }
 
 }
